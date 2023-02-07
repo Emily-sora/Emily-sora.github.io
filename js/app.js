6298961042219920bd7fc241da8d786b833a806e
Emily-sora <sora731@naver.com>
2023-02-07 21:40:10 +0900

    인트로 최초 업로드

---
+++ js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+$(document).ready(function() {
+  new WOW().init();
+  $('#fullpage').fullpage();
+  
+});
+
+$(window).on('load scroll', function() {
+  $('.reveal').each(function() {
+    const scrollTop = $(window).scrollTop();
+    const winH = $(window).height();
+    const elTop = $(this).offset().top;
+    const elH = $(this).outerHeight();
+    const elShow = elTop - winH + elH;
+
+    if(scrollTop >= elShow) {
+      $(this).addClass('on')
+    }
+  }); 
+});
+
+
+
+
+
+
+
+
+
